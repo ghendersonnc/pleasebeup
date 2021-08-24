0c494c1f9d1a7504defbd342cb1c6eaabf44d414
ghendersonnc <ghendersonnc@gmail.com>
2021-08-24 08:12:40 -0400

    initial page creation

---
+++ static/scripts/regex.js
@@ -0,0 +1,15 @@
+function checkInput(event) {
+
+    let url = document.getElementById("url-input").value;
+
+    url = "https://" + url;
+
+    var pattern = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
+
+    if (!pattern.test(url)) {
+        event.preventDefault();
+        alert("Enter a valid URL");
+    }
+
+    return false;
+}
