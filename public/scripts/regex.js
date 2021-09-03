cbf94452bc6843bdc6ab5637caac60a94409a6be
ghendersonnc <ghendersonnc@gmail.com>
2021-09-02 22:40:00 -0400

    Fixed mobile styling

--- public/scripts/regex.js
+++ public/scripts/regex.js
@@ -6,7 +6,6 @@ function checkInput(event) {
         url = "https://" + url;
     }
 
-
     var pattern = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
 
     if (!pattern.test(url)) {
